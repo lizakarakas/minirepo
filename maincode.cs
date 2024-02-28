8d73bc428168f8e081764a4f9e9d66a29e6b4c3b
Ryan Boldwin <161575913+lizakarakas@users.noreply.github.com>
2024-02-28 10:37:35 +0200

    Create maincode.cs

---
+++ maincode.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel;
+using System.Linq;
+using Nuke.Common.Tooling;
+
+[TypeConverter(typeof(TypeConverter<Configuration>))]
+public class Configuration : Enumeration
+{
+    public static Configuration Debug = new Configuration { Value = nameof(Debug) };
+    public static Configuration Release = new Configuration { Value = nameof(Release) };
+
+    public static implicit operator string(Configuration configuration)
+    {
+        return configuration.Value;
+    }
+}
+
+    public ReleaseNotes(Version version, IEnumerable<string> notes, string rawVersionLine)
+        : this(
+            version ?? throw new ArgumentNullException(nameof(version)),
+            new SemVersion(version.Major, version.Minor, version.Build),
+            notes,
+            rawVersionLine)
+    {
+    }
+
+    private ReleaseNotes(Version version, SemVersion semVersion, IEnumerable<string> notes, string rawVersionLine)
+    {
+        Version = version ?? throw new ArgumentNullException(nameof(version));
+        SemVersion = semVersion ?? throw new ArgumentNullException(nameof(semVersion));
+        RawVersionLine = rawVersionLine;
+        _notes = new List<string>(notes ?? Enumerable.Empty<string>());
+    }
